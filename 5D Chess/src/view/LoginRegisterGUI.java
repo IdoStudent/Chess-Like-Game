b22c307a1a681fe8971631c328dfdc46676b20fc
Yasir Fayrooz <30676134+Crazeyheadshot@users.noreply.github.com>
2019-04-21 19:34:09 +1000

    finished login gui

--- 5D Chess/src/view/LoginRegisterGUI.java
+++ 5D Chess/src/view/LoginRegisterGUI.java
@@ -1,54 +1,67 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.Panel;
 import java.awt.TextField;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class LoginRegisterGUI {
 	
 	Label player;
-	Label user = new Label("Username");
-	Label pass = new Label("Password");
+	Label user = new Label("Username: ");
+	Label pass = new Label("Password: ");
 	
-	TextField userTextBox = new TextField(10);
-	TextField passTextBox = new TextField(10);
+	TextField userTextBox = new TextField(15);
+	TextField passTextBox = new TextField(15);
 	
 	Button registerButton = new Button("Register");
 	Button loginButton = new Button("Login");
 	
 	public LoginRegisterGUI(String player)
 	{
-		this.player = new Label(player, SwingConstants.CENTER);
+		this.player = new Label(player + " login:");
 	}
 	
 	public void renderGUI()
 	{
+		passTextBox.setEchoChar('*');
+		
 		JFrame frame = new JFrame();
+		Panel panel = new Panel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		frame.add(panel);
+		
 		
 		JPanel topPanel = new JPanel();
-		topPanel.add(player);
 		topPanel.setLayout(new FlowLayout());
+		topPanel.add(player);
+		panel.add(topPanel);
 		
-		JPanel middlePanel = new JPanel();
-		middlePanel.setLayout(new GridLayout(2,2,5,20));
-		middlePanel.add(user);
-		middlePanel.add(userTextBox);
-		middlePanel.add(pass);
-		middlePanel.add(passTextBox);
+		JPanel usernamePanel = new JPanel();
+		usernamePanel.setLayout(new FlowLayout());
+		usernamePanel.add(user);
+		usernamePanel.add(userTextBox);
+		panel.add(usernamePanel);
 		
-		frame.add(topPanel, BorderLayout.NORTH);
-		frame.add(middlePanel, BorderLayout.CENTER);
+		JPanel passwordPanel = new JPanel();
+		passwordPanel.setLayout(new FlowLayout());
+		passwordPanel.add(pass);
+		passwordPanel.add(passTextBox);
+		panel.add(passwordPanel);
 		
-		//frame.add(registerButton);
-		//frame.add(loginButton);
-		frame.setSize(500,500);
+		JPanel loginRegisterPanel = new JPanel();
+		loginRegisterPanel.add(registerButton);
+		loginRegisterPanel.add(loginButton);
+		panel.add(loginRegisterPanel);
+		
+		frame.setSize(300,200);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
